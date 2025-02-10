0a77150e8f76177f10ce3c360e7d91888c1c7e3c
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-02-10 09:33:02 -0500

    Time: 7504 ms (5.01%), Space: 20.9 MB (5.11%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,25 +4,51 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        result = []
-        nums.sort()
-
+        # Time Complexity = O(n^2)
+        result = set()
+        # Loop through nums with i and j
         for i in range(len(nums)):
-            if i>0 and nums[i]==nums[i-1]:
-                continue
-            j = i+1
-            k = len(nums)-1
-            while(j<k):
-                threeSum = nums[i]+nums[j]+nums[k]
-                if threeSum>0:
-                    k-=1
-                elif threeSum<0:
-                    j+=1
-                else:
-                    result.append([nums[i], nums[j], nums[k]])
-                    j+=1
-                    while j<k and nums[j]==nums[j-1]:
-                        j+=1
-        return result
-                                 
+            hash_set = set()
+            j=i+1
+            while j<len(nums):
+                # Take sum of ith element and jth element
+                sum_ = -(nums[i]+nums[j])
+                # If -sum in hashset
+                if sum_ in hash_set:
+                    # append to result
+                    result.add(tuple(sorted([nums[i], nums[j], sum_])))
+                # add jth element to hashset and move on
+                hash_set.add(nums[j])
+                j+=1   
+        return [list(triplet) for triplet in result]    
+        
+
+
+
+
+        # Time Complexity = O(n^3)
+        # result = []
+        # # Sort nums
+        # nums.sort()
+
+        # # Loop through with i and j from start and k from the end
+        # for i in range(len(nums)):
+        #     # Check if next element same as previous - ignore for duplicates an continue
+        #     if i>0 and nums[i]==nums[i-1]:
+        #         continue
+        #     j = i+1
+        #     k = len(nums)-1
+        #     while (j<k):
+        #         sum_ = nums[i] + nums[j] + nums[k]
+        #         if sum_>0:
+        #             k-=1
+        #         elif sum_<0:
+        #             j+=1
+        #         else:
+        #             result.append([nums[i], nums[j], nums[k]])
+        #             j+=1
+        #             # Check for duplicate
+        #             if j<k and nums[j]==nums[j-1]:
+        #                 j+=1
+        # return result                              
 
