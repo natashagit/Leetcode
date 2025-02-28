41ea8d6362936c2d9b3378caf7dd8a58cf2a133e
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-02-28 09:16:34 -0500

    Time: 0 ms (100%), Space: 12.5 MB (10.62%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         # Setting to 32bit number
         mask = 0xffffffff
-        while (mask&b)>0:
+        while (mask&b)!=0:
             temp = (a&b)<<1
             a = a^b
             b = temp
