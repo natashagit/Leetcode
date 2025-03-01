14cc5d593e80165cb8cf4057d05fc605a93d0662
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-03-01 11:19:50 -0500

    Time: 85 ms (14.67%), Space: 17.7 MB (77.67%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        cnt = 0
+        ans = []
+        for k in range(n+1):
+            while (k>0):
+                if k%2!=0:
+                    cnt+=1
+                k = k/2
+            ans.append(cnt)
+            cnt = 0
+        return ans
+        
