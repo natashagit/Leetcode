7fe454faaf7327a57be3b9c1a691b8ceeeac6ac5
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-03-22 13:45:20 -0400

    Time: 6 ms (88.26%), Space: 14.3 MB (71.58%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,20 +5,43 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        map_dict = {}
-        for num in nums:
-            if num in map_dict:
-                map_dict[num]+=1
+        # Time Complexity: O(nlogk)
+        # Using min heap
+        import heapq # min-heap library
+        from collections import Counter
+        counter = Counter(nums) # creates a dictionary in O(n) with freq
+        heap = []
+        # Push all values with freq onto min heap
+        # Push new val with freq and Pop the value with minimum freq from top of heap until the heap has k elements and then return heap containing k most freq elements
+        for key, val in counter.items():
+            if len(heap)<k:
+                heapq.heappush(heap,(val, key))
             else:
-                map_dict[num]=1
-        freq=[[] for i in range(len(nums)+1)]
-
-        for number, count in map_dict.items():
-            freq[count].append(number)
+                heapq.heappushpop(heap, (val,key))
+        return [h[1] for h in heap]
+            
+        # Time Complexity: O(nlogn)
+        # Create a dict with count for each element in the array
+        dic = {}
+        result = []
+        for i in nums:
+            if i not in dic:
+                dic[i]=1
+            else:
+                dic[i]+=1
         
-        result=[]
-        for i in range(len(freq)-1, 0, -1):
+        # Create empty array and append values in position of count
+        freq = [[] for i in range(len(nums)+1)]
+        for n, count in dic.items():
+            freq[count].append(n)
+        
+        for i in range(len(freq)-1, 0 ,-1):
             for n in freq[i]:
                 result.append(n)
                 if len(result)==k:
                     return result
+
+        
+        
+
+
