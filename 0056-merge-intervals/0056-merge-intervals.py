b6287144204de3be5543f749030c8a690e98fa86
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-04-05 12:05:17 -0400

    Time: 7 ms (96.79%), Space: 16.1 MB (68.73%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -8,15 +8,20 @@ class Solution(object):
         # First gonna sort the intervals
         intervals = sorted(intervals, key=lambda x: x[0])
         result = []
+        # Take first interval for comparison 
         pair = intervals[0]
+        # Iterate through intervals from second
         while i<len(intervals):
+            # If the interval is overlapping, take min and max to merge and update pair
             if pair[1]>=intervals[i][0]:
                 pair[0] = min(pair[0], intervals[i][0])
                 pair[1] = max(pair[1], intervals[i][1])
             else:
+                # No overlap, just append pair to result and update pair to next element
                 result.append(pair)
                 pair = intervals[i]
             i+=1
+        # Append last element in pair
         result.append(pair)
         return result
 
