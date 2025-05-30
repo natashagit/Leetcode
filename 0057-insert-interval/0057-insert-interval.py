b9ca8abe1b87a97867d87944c1db736dbfe8ac90
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-05-30 19:11:48 -0400

    Time: 3 ms (49.98%), Space: 19.8 MB (41.1%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,24 +1,23 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # Add the new interval in
-        intervals.append(newInterval)
-        # Sort the intervals by first element
-        intervals = sorted(intervals, key=lambda x:x[0])
-        # Take first interval for comparison
-        pair = intervals[0]
-        i=1
-        result = []
-        # Iterate through intervals from second
-        while i<len(intervals):
-            # If the interval is overlapping, take min and max to merge and update pair
-            if pair[1]>=intervals[i][0]:
-                pair[0] = min(pair[0], intervals[i][0])
-                pair[1] = max(pair[1], intervals[i][1])
-            else:
-                # No overlap, just append pair to result and update pair to next element
-                result.append(pair)
-                pair = intervals[i]
+        # Since intervals already in ascending, check if second element is smaller than new interval first element
+        i=0
+        result=[]
+        while i<len(intervals) and intervals[i][1]<newInterval[0]:
+            # Left side elements
+            result.append(intervals[i])
             i+=1
-        # Append last element in pair
-        result.append(pair)
+        
+        # New interval for overlapping range
+        while i<len(intervals) and intervals[i][0]<=newInterval[1]:
+            newInterval[0] = min(intervals[i][0], newInterval[0])
+            newInterval[1] = max(intervals[i][1], newInterval[1])
+            i+=1
+        result.append(newInterval)
+
+        #For right side elements
+        while i<len(intervals):
+            result.append(intervals[i])
+            i+=1
+        
         return result
