1305e8951c89f8fc5f953f609729a48f35f3aa94
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-06-02 09:30:32 -0400

    Time: 215 ms (9.75%), Space: 51.4 MB (21.95%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -7,12 +7,12 @@ class Solution:
         # Compare first interval with next to check if overlapping
         for i in range(1, len(intervals)):
             # Check if next interval's start is before prev interval's end
-            if intervals[i][0]<end_prev:
+            if intervals[i][0]>=end_prev:
+                # No overlapping, so move ahead and keep the next interval's endpoint
+                end_prev = intervals[i][1]
+            else:
                 #overlaps so need to remove 1
                 result+=1
                 # Update prev interval's end point by continuing with the interval that has a shorter end point
                 end_prev = min(end_prev, intervals[i][1])
-            else:
-                # No overlapping, so move ahead and keep the next interval's endpoint
-                end_prev = intervals[i][1]
         return result
