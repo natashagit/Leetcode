b03d811c1271697bf14de316a06cfc5253d6ffb1
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-06-02 09:57:51 -0400

    Time: 0 ms (100%), Space: 17.8 MB (40.36%) - LeetHub

--- 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.py
+++ 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.py
@@ -1,15 +1,15 @@
 class Solution:
     def findMiddleIndex(self, nums: List[int]) -> int:
-        rightcumsum = []
-        leftcumsum = []
-        flag = False
+        # Saving total sum of elements in array
+        total = sum(nums)
+        # Initially sum of elements to the left of first element is 0
+        leftsum = 0
         for i in range(len(nums)):
-            rightcumsum.append(sum(nums)-sum(nums[:i]))
-            leftcumsum.append(sum(nums)-sum(nums[i+1:]))
-        for j in range(len(rightcumsum)):
-            if rightcumsum[j]==leftcumsum[j]:
-                flag = True
-                break
-        if flag==False:
-            return -1
-        return j
+            # Sum of elements to the right of element 
+            rightsum = total - leftsum - nums[i]
+            # If the sums are equal return the middle index
+            if leftsum==rightsum:
+                return i
+            # Update the left sum
+            leftsum+=nums[i]
+        return -1
