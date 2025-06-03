e5ba76d1f4ffd3fd007c81e3386544509bc803dd
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-06-03 12:25:21 -0400

    Time: 8 ms (69.66%), Space: 18.2 MB (80.22%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,36 +1,15 @@
-class Solution(object):
-    def maxProduct(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        # Time Complexity = O(n)
-        # Different cases: 
-        # 1. All positive = Even -ve, 2. Odd -ve, 3. Zeroes
-        # For 2. case -> Can be split on odd -ve
-        # For 3. case -> Can be split on zeroes by setting prefix=1
-        max_prod = -float("inf")
-        prefix = 1
-        suffix = 1
-        # Looping from start to end and calculating prefix and max 
-        for i in range(len(nums)):
-            if prefix == 0:
-                prefix = 1
-            prefix = prefix * nums[i]
-            max_prod = max(prefix, max_prod)
-        for i in range(len(nums)-1, -1, -1):
-            if suffix == 0:
-                suffix = 1
-            suffix = suffix * nums[i]
-            max_prod = max(suffix, max_prod)
-        return max_prod
-
-        # Time Complexity = O(n^2)
-        # max_prod = -float("inf")
-        # prod = 1
-        # for i in range(len(nums)):
-        #     prod = nums[0]
-        #     for j in range(1, len(nums)):
-        #         prod = prod * nums[j]
-        #         max_prod = max(prod, max_prod)
-        # return max_prod
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        # To keep track of max and min subarray products in array
+        currMax, currMin = 1,1
+        # Initally result set to max in array
+        result = max(nums)
+        # Loop through nums and calculate max and min of product with self
+        for n in nums:
+            # Storing so can use for currMin
+            tmp = n*currMax
+            currMax = max(n*currMax, n*currMin, n)
+            currMin = min(tmp, n*currMin, n)
+            result = max(result, currMax)
+        
+        return result
