b051a29fe524c23192903ac75439a912c5ca613f
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-06-11 12:35:11 -0400

    Time: 0 ms (100%), Space: 17.9 MB (12.48%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,32 +1,34 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: Optional[ListNode]
-        :type n: int
-        :rtype: Optional[ListNode]
-        """
-        fast = slow = head
-        i = 1
-        if fast==None:
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        if (head is None) or (head.next is None):
             return None
-            
-        while(i<=n):
+        slow = fast = head
+        i=1
+
+        # Move fast pointer to the (N-n)
+        while i<=n:
             fast = fast.next
             i+=1
         
         if fast==None:
             return head.next
-
-        while(fast.next):
-            slow = slow.next
+        
+        # Loop fast till end so that slow reaches 1 node before nth node from end of list
+        while (fast.next):
+            slow=slow.next
             fast = fast.next
-        slow.next=slow.next.next
+            
+        # Removing nth node from end of list
+        slow.next = slow.next.next
 
         return head
         
-        
+
+
+
+            
