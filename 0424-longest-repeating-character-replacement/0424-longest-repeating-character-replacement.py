75241a7186132d33f789ca1ea90926cf6f813197
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-06-12 11:31:54 -0400

    Time: 180 ms (25.48%), Space: 18.2 MB (23.31%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,18 +1,21 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # Sliding window with l and r
-        l=0
-        # Keep a count
+        # Keep a count dictionary for frequency of characters
         count = {}
-        # Keep max length
-        max_length = 0
-        # loop through string with r to extend window
+        # Result to store longest window size
+        result = 0
+        # Left pointer for start of window
+        l=0
+        # Looping through elements in window
         for r in range(len(s)):
-            count[s[r]]= 1+ count.get(s[r], 0)
-            print(count)
-            if (r-l+1)-(max(count.values()))<=k:
-                max_length = max(max_length, r-l+1)
-            else:
+            # Update dictionary with count of elements
+            count[s[r]] = 1 +count.get(s[r], 0)
+            # check if window size - max frequency of element exceeding k (number of replacements)
+            while (r-l+1) - max(count.values())> k:
+                # Decrement first element frequency in count by 1
                 count[s[l]]-=1
+                # If it exceeds, shift l ahead
                 l+=1
-        return max_length
+            # Result contains maximum window size
+            result = max(result, r-l+1)
+        return result
