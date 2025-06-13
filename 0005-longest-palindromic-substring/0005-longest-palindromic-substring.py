74484a12e2fb41a95d00024f94a288febdbcf838
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-06-13 07:44:31 -0400

    Time: 317 ms (52.81%), Space: 17.7 MB (96.21%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,24 +1,24 @@
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        # Time Complexity O(n^3)
-        def ispalindrome(s, left, right):
-            while left<right:
-                if s[left]!=s[right]:
-                    return False
-                left+=1
-                right-=1
-            return True
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        resLen = 0
 
-        n = len(s)
-        for right in range(n, 0, -1):
-            for left in range(n-right+1):
-                if ispalindrome(s, left, left+right-1):
-                    return s[left:left+right] 
-        return ""
+        for i in range(len(s)):
+            # Odd length
+            l, r = i, i 
+            while l>=0 and r<len(s) and s[l]==s[r]:
+                if (r-l+1)>resLen:
+                    res = s[l:r+1]
+                    resLen= r-l+1
+                l-=1
+                r+=1
 
-
-        
+            # Even length
+            l, r = i, i+1
+            while l>=0 and r< len(s) and s[l]==s[r]:
+                if (r-l+1)>resLen:
+                    res = s[l:r+1]
+                    resLen= r-l+1
+                l-=1
+                r+=1
+        return res
