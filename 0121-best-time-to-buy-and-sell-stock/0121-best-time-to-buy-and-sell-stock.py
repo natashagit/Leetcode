aa1ef41f02d4c84688f48664a4dce0a83aa5abad
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-06-20 06:59:28 -0400

    Time: 99 ms (46.7%), Space: 26.8 MB (94.75%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,23 +1,14 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        # Time Complexity = O(n)
-        min_price = prices[0]
-        max_profit = 0
-        for i in range(1, len(prices)):
-            profit = prices[i]-min_price
-            min_price = min(min_price, prices[i])
-            max_profit = max(max_profit, profit)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l = 0
+        r = 1
+        max_profit=0
+        while r<len(prices):
+            if prices[l]<prices[r]:
+                max_profit = max(max_profit, prices[r]-prices[l])
+            else:
+                l=r
+            r+=1
         return max_profit
 
-        # Time Complexity = O(n^2)
-        # n = len(prices)
-        # max_profit = 0
-        # for i in range(n):  # Buying day
-        #     for j in range(i + 1, n):  # Selling day (must be after buying day)
-        #         profit = prices[j] - prices[i]
-        #         max_profit = max(max_profit, profit)
-        # return max_profit
+
