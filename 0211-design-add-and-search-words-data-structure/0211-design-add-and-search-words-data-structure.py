6c7bdbe26ebe4c83ace823faa02a453fc336a223
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-07-03 10:16:24 -0400

    Time: 1364 ms (37.36%), Space: 65.7 MB (50.31%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,27 +1,36 @@
 class TrieNode:
     def __init__(self):
+        # Define structure of trie
         self.children = {}
         self.word = False
 
 class WordDictionary:
 
     def __init__(self):
+        # Initialize trie node
         self.root = TrieNode()
 
     def addWord(self, word: str) -> None:
         curr = self.root
+        # For every character in word
         for c in word:
+            # If not in children add new node or else move ahead
             if c not in curr.children:
                 curr.children[c] = TrieNode()
             curr = curr.children[c]
+        # Set last character flag to True to mark end of word
         curr.word = True
 
     def search(self, word: str) -> bool:
+        # Search depth wise root onwards
         def dfs(j, root):
             curr = root
+            # For every character in word search
             for i in range(j, len(word)):
                 c = word[i]
+                # Character is "."
                 if c==".":
+                    # check in the children values by skipping one ahead
                     for child in curr.children.values():
                         if dfs(i+1, child):
                             return True
