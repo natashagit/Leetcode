2ff7019617e1392d9be44ba78901c680fa86db22
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-09-09 06:19:36 -0400

    Time: 11 ms (34.29%), Space: 12.5 MB (54.12%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -4,33 +4,20 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        for row in range(9):
-            seen = set()
-            for i in range(9):
-                if board[row][i] == ".": 
+        # Creating a set for each
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set) # key = r/3, c/3
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
                     continue
-                if board[row][i] in seen:
+                    # Checking each row, col and square for duplicate
+                if (board[r][c] in rows[r]) or (board[r][c] in cols[c]) or (board[r][c] in squares[(r//3,c//3)]):
                     return False
-                seen.add(board[row][i])
-        
-        for col in range(9):
-            seen = set()
-            for i in range(9):
-                if board[i][col] == ".":
-                    continue
-                if board[i][col] in seen:
-                    return False
-                seen.add(board[i][col])
-            
-        for square in range(9):
-            seen = set()
-            for i in range(3):
-                for j in range(3):
-                    row = (square//3) * 3 + i
-                    col = (square % 3) * 3 + j
-                    if board[row][col] == ".":
-                        continue
-                    if board[row][col] in seen:
-                        return False
-                    seen.add(board[row][col])
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
         return True
+
