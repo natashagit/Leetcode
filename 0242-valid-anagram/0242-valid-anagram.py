c966570e34ef5ab6962206afa2092e5eabab9bbd
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-11-28 15:43:43 -0500

    Time: 23 ms (54.34%), Space: 12.5 MB (96.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,34 +1,17 @@
 class Solution(object):
+    # input: two strings s and t
+    # output: return true if t is an anagram of s
+    # counter from collections
     def isAnagram(self, s, t):
         """
         :type s: str
         :type t: str
         :rtype: bool
         """
-        # Time complexity = O(nlgn) #Space = O(1)
-        # return sorted(s)==sorted(t)
-
-        # Time Complexity = O(s+t) # Space= extra for 2 dicts
-        if len(s)!=len(t):
-            return False
-        count_s = {}
-        count_t = {}
-        for i in range(len(s)):
-            if s[i] in count_s:
-                count_s[s[i]]+=1
-            else:
-                count_s[s[i]]=1
-        
-        for i in range(len(t)):
-            if t[i] in count_t:
-                count_t[t[i]]+=1
-            else:
-                count_t[t[i]]=1
-        
-        for key in count_s:
-            if count_s[key]!=count_t.get(key,0):
-                return False
-        return True
-        
-
+        from collections import Counter
+        t_counter = Counter(t)
+        s_counter = Counter(s)
+        if t_counter==s_counter:
+            return True
+        return False
         
