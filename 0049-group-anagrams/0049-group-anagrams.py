acd59740827a2923a76b6fe23a66cbf6dcaacefa
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-11-28 16:03:31 -0500

    Time: 35 ms (21.35%), Space: 16.1 MB (81.08%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,22 @@
 class Solution(object):
+    # input: array of strings strs
+    # output: list of lists having similar anagrams together
+    # loop through the list of strs
+    # have a dict to store the key as sorted(str) with the values as the str
+    # keep checking if sorted(str) already present in the dict, then just append the str to the values
+    # return the list of all the values
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        map_dict={}
-        for string in strs:
-            str_key = "".join(sorted(string))
-            if str_key in map_dict:
-                map_dict[str_key].append(string)
+        dict_strs = {}
+        for i in strs:
+            if ''.join(sorted(i)) in dict_strs:
+                dict_strs[''.join(sorted(i))].append(i)
             else:
-                map_dict[str_key]= [string]
-        return list(map_dict.values())
-            
+                dict_strs[''.join(sorted(i))]=[i]
+        return list(dict_strs.values())
 
+
+        
