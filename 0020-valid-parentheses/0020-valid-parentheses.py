1c7c21de038f3cdf40f595c538245bd0ddeaaf73
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-12-07 15:49:06 -0500

    Time: 8 ms (8.7%), Space: 12.5 MB (91.1%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,16 +1,38 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        dict_parenthesis = {')':'(',']':'[','}':'{'}
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # input: string of brackets
+        # output: True/False if every open bracket has a closed one
+        # stacks
+        # dict -> save relationships ')':'('
+        # loop through string
+        # check if char in values
+            # if so then add to stack
+        # if not
+            # check if stack empty
+                # if empty-> return False
+                # if not->check if dict[char] is stack.pop
+                    # if not-> return False
+        # if stack is empty
+            # return True
+        dict_brack = {')':'(', '}':'{', ']':'['}
         stack = []
-
         for char in s:
-            if char in dict_parenthesis:
-                if stack and stack[-1]==dict_parenthesis[char]:
-                    stack.pop()
-                else: 
-                    return False
-            else:
+            if char in dict_brack.values():
                 stack.append(char)
+            else:
+                if len(stack)==0:
+                    return False
+                else:
+                    if dict_brack[char]==stack[-1]:
+                        stack.pop()
+                    else:
+                        return False
         if stack:
             return False
         return True
+
+                    
