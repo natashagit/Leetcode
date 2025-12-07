527106cd7dd3dda02f29128e6d56a45d296a5974
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-12-07 01:54:28 -0500

    Time: 14 ms (69.79%), Space: 13.5 MB (43.88%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,28 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # input: string, which needs to be cleaned up
+        # output: true/false 
+        # edge case: one letter, empty-> true
+
+        # preprocess string by removing non alphanumeric chars
+        # left point and right pointer at start and end of string
+        # loop through till left!=right pointer
+        # keep checking if the value is not equal-> return False
+        # In the end return True
+
+        s = "".join([i for i in s if i.isalnum()])
         s = s.lower()
-        s = [i for i in s if i.isalnum()]
-        l = 0
-        r = len(s)-1
-        while l<r:
-            if s[l]!=s[r]:
+        if len(s)<=1:
+            return True
+        left = 0
+        right = len(s)-1
+        while left<right:
+            if s[left]!=s[right]:
                 return False
-            l+=1
-            r-=1
+            left+=1
+            right-=1
         return True
