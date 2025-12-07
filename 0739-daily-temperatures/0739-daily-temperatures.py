4456d1973e6a6415d45b062c9550124fe6694b1e
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-12-07 18:08:54 -0500

    Time: 167 ms (55.67%), Space: 26.9 MB (50.98%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,17 +1,21 @@
-class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # Initialize result array for storing index/number of days
-        result = [0]*len(temperatures)
-        i = 0
-        stack = [] # to store temperature and index [t,i]
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        # input: list of temperatures
+        # output: list of number of days to wait for warmer temp
 
-        for i,t in enumerate(temperatures):
-            # if next temperature greater, then pop previous
-            while stack and t>stack[-1][0]:
-                # if next temperature is greater, pop previous one
-                stackt, stacki = stack.pop()
-                # store the number of days between previous and new in result
-                result[stacki] = i - stacki
-            # if next temperature smaller, keep appending to stack
-            stack.append([t,i])
-        return result
+        # stack = []
+        # answer = [0]*len(temperatures)
+        
+        stack = []
+        answer = [0]*len(temperatures)
+        for i in range(len(temperatures)):
+            while stack and temperatures[i]>temperatures[stack[-1]]:
+                prev = stack.pop()
+                answer[prev] = i-prev
+            stack.append(i)
+        
+        return answer
