9e7d1b8646aab1d5ffde61525c60578afc41ee41
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-12-07 16:58:44 -0500

    Time: 11 ms (41.19%), Space: 13.7 MB (65.58%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,23 +1,33 @@
-class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        # popped element is right operand
+        # input: list of strings
+        # output: evaluated value
+        # stack to put all the numbers
+
+        # loop through tokens
+        # if token is a number push to stack
+        # if its an operand then pop two elements from stack
+        # evaluate value and push back onto stack
+        # finally return stack[-1]
         stack = []
-        for i in range(len(tokens)):
-            if tokens[i]=="*":
-                a = stack.pop()
-                b = stack.pop()
-                stack.append(a*b)
-            elif tokens[i]=="+":
-                a = stack.pop()
-                b = stack.pop()
-                stack.append(a+b)
-            elif tokens[i]=="-":
-                a = stack.pop()
-                b = stack.pop()
-                stack.append(b-a)
-            elif tokens[i]=="/":
-                a = stack.pop()
-                b = stack.pop()
-                stack.append(int(float(b)/a))
+        for t in tokens:
+            if t.lstrip('-').isdigit():
+                stack.append(int(t))
             else:
-                stack.append(int(tokens[i]))
-        return stack[0]
+                a = stack.pop()
+                b = stack.pop()
+                if t=='+':
+                    stack.append(b+a)
+                elif t=='*':
+                    stack.append(b*a)
+                elif t=='-':
+                    stack.append(b-a)
+                else:
+                    stack.append(int(float(b)/a))
+        if stack:
+            return stack[-1]
