3e3c300e7fdc5084afc6ff2db89e5687fa3d4c62
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-12-29 10:31:27 -0500

    Time: 0 ms (100%), Space: 12.6 MB (29.3%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,21 +1,37 @@
-class Solution:
-    def findMin(self, nums: List[int]) -> int:
-        min_val = float("inf")
-        low = 0
-        high = len(nums)-1
-        # Find mid point dynamically
-        while (low<=high):
-            mid = int((low+high)/2)
-            # Check if left half or right half is sorted
-            if nums[mid]>=nums[low]:
-                # Left half sorted
-                min_val = min(min_val, nums[low])
-                # Took minimum value in left half, now move to right half
-                low = mid+1
-            else:
-                # Right half sorted
-                min_val = min(min_val, nums[mid])
-                high =mid-1
-        return min_val
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # input: rotated sorted array
+        # output: minimum element in that rotated sorted array
 
+        # binary search
+        # we can return the minimum in the sorted part of the array
+        # set min_value to nums[0]
+        # we can set lowptr as 0
+        # we can set highptr as len(nums)
+        # we can take the midpt -> take lower value
+        # check if the midpt is higher than lowptr value
+            # set the min_value as low's value
+            # set low to midpt+1
+        # if no, then
+            # set the min_value as midpt's value
+            # set high to midpt-1
+        # return min_value
+            # 4 5 6 0 1 2 
         
+        min_value = nums[0]
+        lowptr = 0
+        highptr = len(nums)-1
+
+        while lowptr<=highptr:
+            midpt = (lowptr+highptr)//2
+            if nums[midpt]>=nums[lowptr]:
+                min_value = min(min_value, nums[lowptr])
+                lowptr = midpt+1
+            else:
+                min_value = min(min_value, nums[midpt])
+                highptr = midpt-1
+        return min_value
