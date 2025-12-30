0fd38c03c8d1a25f222db602a54dd8c720020ea4
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-12-30 10:55:21 +0530

    Time: 31 ms (30.15%), Space: 13.3 MB (38.85%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,21 +1,23 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        # Sliding window
-        # Use a set for unique elements
-        charSet = set()
-        # Left iterator to remove element from start
-        l = 0
-        # Store max length of substring
-        res = 0
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # input: string
+        # output: length of longest substring without duplicates
 
-        # Iterate through string s
-        for r in range(len(s)):
-            while s[r] in charSet:
-                charSet.remove(s[l])
+        # sliding window keeping only the substring with non duplicate chars
+        # keep count of longest substring
+        # store into a dictionary and if the count of any char>1
+            # reduce count of first element from dict until count==1
+        max_length = 0
+        str_set = set()
+        l=0
+        for i in range(len(s)):
+            while s[i] in str_set:
+                str_set.remove(s[l])
                 l+=1
-
-            # Add element to charSet
-            charSet.add(s[r])
-            # Store maximum length of substring in res
-            res = max(res, r-l+1)
-        return res
+            str_set.add(s[i])
+            max_length = max(max_length, i-l+1)
+        return max_length
