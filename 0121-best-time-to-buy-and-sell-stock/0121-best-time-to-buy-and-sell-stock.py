eb346fe180652243b322d49891a57c38cc0a968f
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2025-12-29 23:32:00 -0500

    Time: 91 ms (47.67%), Space: 19.6 MB (10.71%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,24 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        # input: prices array
+        # output: max profit made when selling at high and buying at low
+
+        # sliding window
+        # initialize max profit
+        # left ptr at 0
+        # right ptr starting from 1
+        # if value at right ptr>value at left ptr
+            # calculate profit and save to maxprofit
+        # return maxprofit
         l = 0
-        r = 1
-        profit = 0
         max_profit = 0
-        while r<len(prices):
-            if prices[l]<prices[r]:
-                profit = prices[r]-prices[l]
-                max_profit = max(profit, max_profit)
+        for r in range(1, len(prices)):
+            if prices[r]>prices[l]:
+                max_profit = max(max_profit, prices[r]-prices[l])
             else:
-                l=r
-            r+=1
+                l = r
         return max_profit
-
-
-
