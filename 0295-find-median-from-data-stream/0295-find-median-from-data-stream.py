94c681aab2c1b2eb3a0fa29625245dc112c700be
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-01-13 16:07:06 +0530

    Time: 1030 ms (35.81%), Space: 36 MB (16.83%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,31 +1,32 @@
-class MedianFinder:
+class MedianFinder(object):
 
     def __init__(self):
-        self.small, self.large = [], []
+        self.left = []
+        self.right = []
 
-    def addNum(self, num: int) -> None:
-        # Made the number negative because its a maxheap, by default min heap
-        heapq.heappush(self.small, -1*num)
-        # To make sure all nums in small <= nums in large
-        if (self.small and self.large and (-1*self.small[0])>self.large[0]):
-            # pop actual value from small and put in large
-            val = -1*heapq.heappop(self.small)
-            heapq.heappush(self.large, val)
+    def addNum(self, num):
+        """
+        :type num: int
+        :rtype: None
+        """
+        # add to left heap which is a max heap
+        heapq.heappush(self.left, -num)
 
-        # Uneven size of small and large
-        if len(self.small)>len(self.large)+1:
-            val = -1*heapq.heappop(self.small)
-            heapq.heappush(self.large, val)
-        if len(self.large)>len(self.small)+1:
-            val = heapq.heappop(self.large)
-            heapq.heappush(self.small, -1*val)
-        
-    def findMedian(self) -> float:
-        if len(self.small)>len(self.large):
-            return -1*self.small[0]
-        if len(self.large)>len(self.small):
-            return self.large[0]
-        return (-1*self.small[0]+self.large[0])/2
+        # right heap always has values>left heap
+        # push largest of left heap to right heap to keep that order
+        heapq.heappush(self.right, -heapq.heappop(self.left))
+
+        # if right has more elements move smallest back to left
+        if len(self.right)>len(self.left):
+            heapq.heappush(self.left, -heapq.heappop(self.right))
+
+    def findMedian(self):
+        """
+        :rtype: float
+        """
+        if len(self.left)>len(self.right):
+            return float(-self.left[0])
+        return (-self.left[0]+self.right[0])/2.0
 
 
 # Your MedianFinder object will be instantiated and called as such:
