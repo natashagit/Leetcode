3ee47353575f2dde9df4ff679cb8fc5e008efec1
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-01-21 12:29:57 -0500

    Time: 218 ms (77.9%), Space: 28 MB (50.99%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,37 +1,29 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        # input: mXn matrix
+        # output: number of islands
+        # number of components
+        # DFS and everytime it breaks, increase count of islands
+        r,c = len(grid), len(grid[0])
+
+        def dfs(i,j):
+            if i<0 or i>=r or j<0 or j>=c or grid[i][j]!='1':
+                return
+            # convert to water since not visiting again
+            grid[i][j]='0'
+            dfs(i, j+1)
+            dfs(i+1, j)
+            dfs(i,j-1)
+            dfs(i-1, j)
         
-        rows, cols = len(grid), len(grid[0])
-        # Keep count of locations visited
-        visit = set()
-        islands = 0
-
-        def bfs(r,c):
-            # vertical and horizontal from r,c
-            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-            from collections import deque
-            q = deque()
-            visit.add((r,c))
-            q.append((r , c))
-            while q:
-                row, col = q.popleft()
-                for dr, dc in directions:
-                    #index of row and col in directions
-                    rr, cc = row+dr, col+dc
-                    #check if row, col in range, element is 1, not visited
-                    if (rr in range(rows) and cc in range(cols) and grid[rr][cc]=="1" and (rr,cc) not in visit):
-                        visit.add((rr,cc))
-                        q.append((rr, cc))
-
-
-        # loop through grid
-        for r in range(rows):
-            for c in range(cols):
-                # check for 1 in grid and if not visited, do bfs to get all 1s of that island and increment number of islands
-                if grid[r][c]=="1" and (r,c) not in visit:
-                    bfs(r,c)
-                    islands+=1
-        
-        return islands
+        num_islands = 0
+        for i in range(r):
+            for j in range(c):
+                if grid[i][j]=='1':
+                    num_islands+=1
+                    dfs(i,j)
+        return num_islands
