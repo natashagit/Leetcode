21d479e38b5a9bd0bde4787bdfb72b642e7bc4f8
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-01-21 16:42:31 -0500

    Time: 7 ms (78.59%), Space: 16.3 MB (17.33%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,39 +1,28 @@
-class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = [[] for _ in range(numCourses)]
-        for course, pre in prerequisites:
-            #directed edge in adj list
-            adj[course].append(pre)
-        
-        # courses visiting
-        visited = [False]*numCourses
-        # detect cycle
-        onepath = [False]*numCourses
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        prereqs = defaultdict(list)
+        for c,p in prerequisites:
+            prereqs[c].append(p)
 
-        def dfs(course):
-            # if course already true- visited - loop detected
-            if onepath[course]:
-                return False
-            # course can be completed if it does not have prereqs
-            if visited[course]:
+        # Detect if there is a cycle
+        def cycle(course, seen):
+            if course in seen:
                 return True
-            # Mark as detected
-            onepath[course] = True
+            seen.add(course)
+            for p in prereqs[course]:
+                if cycle(p, seen):
+                    return True
+            prereqs[course] = []
+            seen.remove(course)
+            return False
 
-            # Loop through all prereqs
-            for pre in adj[course]:
-                if not dfs(pre):
-                    return False
-
-            # Mark as visited
-            visited[course] = True
-            # Reset to false
-            onepath[course] = False
-            return True
-
-        # Running dfs on courses
+        seen = set()
         for course in range(numCourses):
-            if not dfs(course):
+            if cycle(course,seen):
                 return False
         return True
-
