ae6649c0affaca7fd3780552de70e61a07c4aabe
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-01-21 13:16:30 -0500

    Time: 244 ms (36.19%), Space: 34.5 MB (5.02%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -9,12 +9,12 @@ class Solution(object):
         # number of components
         # DFS 
         r,c = len(grid), len(grid[0])
-
+        visit = set()
         def dfs(i,j):
-            if i<0 or i>=r or j<0 or j>=c or grid[i][j]!='1':
+            if i<0 or i>=r or j<0 or j>=c or grid[i][j]!='1' or (i,j) in visit:
                 return
             # convert to water since not visiting again
-            grid[i][j]='0'
+            visit.add((i,j))
             dfs(i, j+1)
             dfs(i+1, j)
             dfs(i,j-1)
@@ -23,7 +23,7 @@ class Solution(object):
         num_islands = 0
         for i in range(r):
             for j in range(c):
-                if grid[i][j]=='1':
+                if grid[i][j]=='1' and (i,j) not in visit:
                     num_islands+=1
                     dfs(i,j)
         return num_islands
