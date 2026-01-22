49077f44c8d5d651df525cf6444071123a461026
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-01-22 11:51:33 -0500

    Time: 285 ms (52.65%), Space: 13.3 MB (48.81%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
+        # input: beginword, endword, list of words
+        # output: length of shortest sequence following the instructions
+        # each word is a node, an edge exists if they have only one different letter
+        # shortest path in the graph-bfs
+        from collections import deque
+        q = deque()
+        # keep count for path length
+        q.append((beginWord,1))
+        wordSet = set(wordList)
+        if endWord not in wordSet:
+            return 0
+        visited = set(beginWord)
+        while q:
+            word, level = q.popleft()
+            if word==endWord:
+                return level
+            for i in range(len(word)):
+                for j in "abcdefghijklmnopqrstuvwxyz":
+                    new_word = word[:i]+j+word[i+1:]
+                    if new_word in wordSet and new_word not in visited:
+                        visited.add(new_word)
+                        q.append((new_word,level+1))
+        
+        return 0
+
+
