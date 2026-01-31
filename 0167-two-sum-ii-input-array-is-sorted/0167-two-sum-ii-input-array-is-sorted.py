555385324f9918c7a22a538514b2e9fa18d2b6f8
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-01-31 16:53:40 -0500

    Time: 4 ms (74.61%), Space: 12.9 MB (43.19%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -5,14 +5,21 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        # input: sorted numbers array, target sum
+        # output: return [index1+1, index2+1] of values adding up to target
+        # binary search
+        # two ptrs, from start and end
+        # while start<end
+        # if sum of the two > target-> reduce end
+        # else increment start
+        # else return [start+1, end+1]
         start = 0
         end = len(numbers)-1
-
         while start<end:
-            if (numbers[start]+numbers[end])<target:
-                start+=1
-            elif (numbers[start]+numbers[end])>target:
+            if numbers[start]+numbers[end]>target:
                 end-=1
+            elif numbers[start]+numbers[end]<target:
+                start+=1
             else:
                 return [start+1, end+1]
         return []
