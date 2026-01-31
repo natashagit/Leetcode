2ba4e5aebad9894f91d3adc22ee5e8827ec5a6a3
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-01-31 16:44:43 -0500

    Time: 0 ms (100%), Space: 13.3 MB (32.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,26 +1,29 @@
 class Solution(object):
-    # input: nums array, target
-    # output: indices of two values in nums that add up to target
-    # dictionary -> value: index
-    # we check if the value subtracted from the target is there in the dictionary, if so we retur, the value for that key in the dict and the current index
-    # if the index!=the value: same element
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        # input: nums array, target
-        # output: index of two numbers that add up to target
-        # dictionary
-        # init dict
-        # loop through array
-        # if target-nums[i] in dict then return i and dict[num[i]]
-        # else add dict[nums[i]]=i to dict
-        dict_num={}
+        # input: nums, target
+        #   output: indices of two values in nums that add up to target
+        # constraints: can have negative and 0
+        # edge cases: same number - different indices, negative numbers
+        # dictionary to store the elements seen with its index
+        # loop through nums and check if the target-element in nums is there in dictionary
+        # if its there then we want to return the index and the value of the item in the dictionary
+        # else add to dictionary
+
+        dict_nums = {}
         for i in range(len(nums)):
-            if target-nums[i] in dict_num:
-                return [i, dict_num[target-nums[i]]]
-            else:
-                dict_num[nums[i]]=i
+            if target-nums[i] in dict_nums:
+                return [i, dict_nums[target-nums[i]]]
+            dict_nums[nums[i]]=i
         
+
+
+
+
+
+
+    
