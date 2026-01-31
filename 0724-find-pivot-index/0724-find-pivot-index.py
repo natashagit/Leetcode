98732a9475e814e3754be1016b025416f050bbd8
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-01-31 17:17:21 -0500

    Time: 3 ms (87.05%), Space: 13.3 MB (59.38%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -5,13 +5,20 @@ class Solution(object):
         :rtype: int
         """
         # input: nums array
-        # output: pivot index
-        total = sum(nums)
+        # output: pivot index which equally divides with sum equal on both sides of pivot, else -1
+        # constraints: array size 1, negative numbers
+        # edge cases: one element, index return 0
+        # take sum of all elements
+        # left sum assign to 0
+        # loop through nums
+        # check if sum-left sum-pivot == left sum
+        # if yes then return pivot
+        # else increase left sum by adding element to it
+
         left_sum = 0
+        total_sum = sum(nums)
         for i in range(len(nums)):
-            if left_sum == total-left_sum-nums[i]:
+            if total_sum-left_sum-nums[i] == left_sum:
                 return i
             left_sum+=nums[i]
-        
         return -1
-        
