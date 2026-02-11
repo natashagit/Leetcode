68b7919c2a3b73f125db2584f22f3710771ef41c
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-02-11 13:18:35 -0500

    Time: 115 ms (68.21%), Space: 20.9 MB (21.04%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,25 +4,29 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        # input: list of heights
-        # output: max area between two edges
-        # area= difference btwn the edges*height of the shorter edge
-        # two pointer approach
-        # left and right pointer
-        # max_area initialized
-        # while left< right
-        # if height of left<height of right-> area formula=heigh of left*(right-left pointer) -> store max_area
-        # return max_area
-
+        # input: heights list
+        # output: max area between two heights in heights
+        # left and right ptrs
+        # area = maximize->min height(left height, right height)*right-left+1
+        # if left height<right height->left+=1
+        # else right-=1
         left = 0
         right = len(height)-1
-        max_area = 0
+        max_area = float("-inf")
         while left<right:
+            max_area = max(max_area, (right-left)*min(height[left], height[right]))
             if height[left]<height[right]:
-                area = height[left]*(right-left)
                 left+=1
             else:
-                area = height[right]*(right-left)
                 right-=1
-            max_area = max(area, max_area)
         return max_area
+
+
+
+
+
+
+
+
+
+       
