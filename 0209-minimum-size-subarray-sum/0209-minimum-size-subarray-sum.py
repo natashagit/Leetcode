3053cd7ca19729007dcee3d36dc6895c9062561d
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-02-11 12:44:25 -0500

    Time: 13 ms (64.69%), Space: 20.2 MB (40.93%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -6,17 +6,25 @@ class Solution(object):
         :rtype: int
         """
         # input: nums, target
-        # output: minimum length of subarray whose sum is<=target
+        # output: minimum length of subarray whose sum is>=target
+        # nums all elements are positive
+        # two pointer approach and keep window
+        # left at start
+        # right move through nums
+        # if the sum upto right>=target -> shrink window from left and keep count of the size-> r-l 
         left = 0
         curr_sum = 0
-        min_len = float("inf")
+        result_len = float("inf")
         for right in range(len(nums)):
             curr_sum+=nums[right]
             while curr_sum>=target:
-                min_len = min(min_len, right-left+1)
+                result_len = min(result_len, right-left+1)
                 curr_sum-=nums[left]
                 left+=1
-        if min_len==float("inf"):
+        if result_len==float("inf"):
             return 0
         else:
-            return min_len
+            return result_len
+                
+        
+        
