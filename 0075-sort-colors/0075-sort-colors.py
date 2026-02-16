f2a7de7bb7c2e7db734a73793f5f1f7f818f5b5a
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-02-16 14:44:47 -0500

    Time: 0 ms (100%), Space: 12.5 MB (19.98%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,27 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        # Using two pointers white and blue
-        red = 0
-        white = 0
-        blue = len(nums)-1
-        # red, white, blue colors = 0,1,2
-        while white<=blue:
-            # Check if white is 0 and red is 1 then switch the two
-            if nums[white]==0:
-                if nums[red]!=0:
-                    nums[red], nums[white] = nums[white], nums[red]
-                white+=1
-                red+=1
-            # If white is 1, then just move ahead
-            elif nums[white]==1:
-                white+=1
-            # If white is 2, then switch with blue if blue is not 2
-            else:
-                if nums[blue]!=2:
-                    nums[white], nums[blue] = nums[blue], nums[white]
-                blue-=1
-        return nums 
+        # input: array of 3 colored elements
+        # output: sorted together inorder of 0, 1, 2
+        # loop through with i
+        # L and R pointer - shift all elements to left of L that are 0 and all elements to right of R that are 2
 
-
-        
+        L = 0
+        R = len(nums)-1
+        i = 0
+        while i<=R:
+            if nums[i]==0:
+                nums[L], nums[i]=nums[i], nums[L]
+                L+=1
+            elif nums[i]==2:
+                nums[R], nums[i]=nums[i], nums[R]
+                R-=1
+                i-=1
+            i+=1
+        return nums
