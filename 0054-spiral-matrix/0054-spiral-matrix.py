5d9cac69eba4465bbc668ed2e08e947d73813a28
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-02-17 17:44:47 -0500

    Time: 0 ms (100%), Space: 12.4 MB (57.74%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -4,28 +4,35 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
+        # input 2D matrix
+        # output: 1D list of spiral
+        # 1d array list, 
+        # while top, bottom, right, left boundaries keep reducing
+        # add elements to result array
+        # from left to right and top to bottom
+
         m = len(matrix)
         n = len(matrix[0])
-        left, right = 0, n-1
-        top, bottom = 0, m-1 
+        top = 0
+        bottom = m-1
+        left = 0
+        right = n-1
         result = []
-        while(top<=bottom and left<=right):
+        while top<=bottom and left<=right:
             for i in range(left, right+1):
                 result.append(matrix[top][i])
             top+=1
             for i in range(top, bottom+1):
                 result.append(matrix[i][right])
             right-=1
-            # Check needed if only one line, top increased
+            # check if line present as top increased
             if top<=bottom:
                 for i in range(right, left-1, -1):
                     result.append(matrix[bottom][i])
                 bottom-=1
-            # Check needed if no top line to be printed in end
+            # check if line present as right reduced
             if left<=right:
                 for i in range(bottom, top-1, -1):
                     result.append(matrix[i][left])
                 left+=1
         return result
-        
-        
