fafc1626c93746d0b68c0f998c6208534e2689eb
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-02-17 19:40:43 -0500

    Time: 35 ms (67.75%), Space: 21 MB (61.37%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -4,20 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # input: nums array
+        # output: repeated value
+        # sorting would make it O(nlogn) time but modify array
+        # using set would make it O(n) time and space
+        # need to keep at O(1) space
+        # if I treat each index as a node and check the next as the nums[index]
+        # it would loop withing 1..n
+        # would revisit one again as its repeated - forming a cycle
+        # the entry point of the cycle is the duplicate value
 
-        # first detect cycle
-        slow = fast = 0
+        fast = slow = 0
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
-            if slow == fast:
+            if slow==fast:
                 break
-
-        # Detect start of cycle for repeated number
-        pointer = 0
+        start = 0
         while True:
-            pointer = nums[pointer]
+            start = nums[start]
             slow = nums[slow]
-            if slow == pointer:
+            if slow==start:
                 return slow
+
+
         
