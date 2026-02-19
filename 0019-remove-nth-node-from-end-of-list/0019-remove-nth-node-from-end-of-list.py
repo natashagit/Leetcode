137157a1f83aa260d7a7d7f09c7aec0d495d1fd0
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-02-19 01:10:17 -0500

    Time: 0 ms (100%), Space: 12.6 MB (30.11%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -13,20 +13,19 @@ class Solution(object):
         # input: ll and nth node from end of list to remove [1,2,3,4,5], 2
         # output: ll with n'th node from end removed [1,2,3,5]
         # edge cases: only one node, or empty list, or n more than length of ll
-
         # use slow and fast pointer
         # move fast pointer to nth node from start with a loop
         # start a slow pointer to move from start till the fast pointer reaches the end
         # slow pointer will reach a node before nth node from end
-
-        slow = fast = head
-        if slow is None or slow.next is None:
+        
+        dummy= head
+        if dummy is None or dummy.next is None:
             return None
-        i=0
+        slow = fast = head
+        i = 0
         while i<n:
             fast = fast.next
             i+=1
-        
         if fast is None:
             return head.next
 
@@ -35,5 +34,5 @@ class Solution(object):
             fast = fast.next
         
         slow.next = slow.next.next
-        return head
 
+        return head
