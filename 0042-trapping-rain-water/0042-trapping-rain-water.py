2ef33b3b3562196c7ca5d8cd9c135c2130e00448
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-02-25 15:23:47 -0500

    Time: 33 ms (17.67%), Space: 14 MB (23.09%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -4,23 +4,30 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        # input: heights list
-        # output: sum of area of water filled
-        # calculate max_left and max_right at every point
-        # calculate area at point by taking min of max_left and max_right and subtract the current height from it to get area
-        max_left = [0]*len(height)
-        max_right = [0]*len(height)
+        # input: list of heights
+        # output: total area of water trapped in between
+        # height-> 0, -ve, one one element in height
+        # input size->10^4 O(n)
+        # water limited by tallest wall on left or right of it
+        # min of maxleft, maxright at every point will determine the area at that point
+        # keep an array to store maxleft elements
+        # keep an array to store maxright elements
+        # create maxleft array by looping from start and storing maxvalue seen at every index
+        # create maxright array by looping form end and storing maxvalue seen at every index
+        # finally calculate the area by summing up the area at every index by taking the minimum of maxleft and maxright and reducing the current index height from it
 
-        max_left[0] = height[0]
-        max_right[len(height)-1]=height[len(height)-1]
-
-        for i in range(1, len(height)):
-            max_left[i] = max(height[i], max_left[i-1])
-        
-        for i in range(len(height)-2, -1, -1):
-            max_right[i] = max(height[i], max_right[i+1])
-            
+        maxLeft = [0]*len(height)
+        maxRight = [0]*len(height)
+        maxLeft[0] = height[0]
+        maxRight[len(height)-1] = height[len(height)-1]
         area = 0
+
         for i in range(len(height)):
-            area += min(max_left[i], max_right[i])-height[i]
+            maxLeft[i] = max(maxLeft[i-1], height[i])
+        for i in range(len(height)-2, -1, -1):
+            maxRight[i] = max(maxRight[i+1], height[i])
+        
+        for i in range(len(height)):
+            area+=min(maxLeft[i], maxRight[i])-height[i]
+
         return area
