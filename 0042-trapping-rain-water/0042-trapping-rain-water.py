019278288326e672b398f024807c566cb8dcbe2d
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-02-25 15:24:46 -0500

    Time: 28 ms (23.24%), Space: 14.4 MB (7.94%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -22,7 +22,7 @@ class Solution(object):
         maxRight[len(height)-1] = height[len(height)-1]
         area = 0
 
-        for i in range(len(height)):
+        for i in range(1, len(height)):
             maxLeft[i] = max(maxLeft[i-1], height[i])
         for i in range(len(height)-2, -1, -1):
             maxRight[i] = max(maxRight[i+1], height[i])
