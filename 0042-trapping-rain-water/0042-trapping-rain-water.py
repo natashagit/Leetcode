cbb441cf3af11d7178e9d7b97f9bb32869c144a6
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-02-25 15:36:40 -0500

    Time: 6 ms (88.88%), Space: 13.5 MB (84.49%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -15,20 +15,44 @@ class Solution(object):
         # create maxleft array by looping from start and storing maxvalue seen at every index
         # create maxright array by looping form end and storing maxvalue seen at every index
         # finally calculate the area by summing up the area at every index by taking the minimum of maxleft and maxright and reducing the current index height from it
-        if len(height)<=1:
-            return 0
-        maxLeft = [0]*len(height)
-        maxRight = [0]*len(height)
-        maxLeft[0] = height[0]
-        maxRight[len(height)-1] = height[len(height)-1]
-        area = 0
-
-        for i in range(1, len(height)):
-            maxLeft[i] = max(maxLeft[i-1], height[i])
-        for i in range(len(height)-2, -1, -1):
-            maxRight[i] = max(maxRight[i+1], height[i])
+        # TC: O(n) and SC: O(n)
         
-        for i in range(len(height)):
-            area+=min(maxLeft[i], maxRight[i])-height[i]
+        # if len(height)<=1:
+        #     return 0
+        # maxLeft = [0]*len(height)
+        # maxRight = [0]*len(height)
+        # maxLeft[0] = height[0]
+        # maxRight[len(height)-1] = height[len(height)-1]
+        # area = 0
 
+        # for i in range(1, len(height)):
+        #     maxLeft[i] = max(maxLeft[i-1], height[i])
+        # for i in range(len(height)-2, -1, -1):
+        #     maxRight[i] = max(maxRight[i+1], height[i])
+        
+        # for i in range(len(height)):
+        #     area+=min(maxLeft[i], maxRight[i])-height[i]
+        # return area
+
+
+        # SC: O(1) soln
+        l = 0 
+        r = len(height)-1
+        maxleft = maxright = 0
+        area = 0
+        while l<r:
+            if height[l]<=height[r]:
+                if height[l]>=maxleft:
+                    maxleft = height[l]
+                else:
+                    area+=maxleft-height[l]
+                l+=1
+            else:
+                if height[r]>=maxright:
+                    maxright = height[r]
+                else:
+                    area+=maxright-height[r]
+                r-=1
         return area
+            
+
