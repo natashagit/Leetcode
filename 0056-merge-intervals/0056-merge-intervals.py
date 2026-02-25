7d982d7bf9d28a9b65fc49c25b479dd1b0147888
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-02-25 16:02:57 -0500

    Time: 13 ms (28.95%), Space: 16.3 MB (45.1%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,28 +1,29 @@
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        i = 1
-        # First gonna sort the intervals
-        intervals = sorted(intervals, key=lambda x: x[0])
-        result = []
-        # Take first interval for comparison 
-        pair = intervals[0]
-        # Iterate through intervals from second
-        while i<len(intervals):
-            # If the interval is overlapping, take min and max to merge and update pair
-            if pair[1]>=intervals[i][0]:
-                pair[0] = min(pair[0], intervals[i][0])
-                pair[1] = max(pair[1], intervals[i][1])
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        # input: intervals [[a,b], [c,d]]
+        # output: merged intervals [[a,d]]
+        # O(nlogn)
+        # if no intervals, return []
+        # set merged=[]
+        # sort all intervals by start time
+        # add first interval to merged
+        # loop through intervals from 2nd onwards
+        # now check if end of interval in merged>=end of current interval
+        # if yes-> overlaps: update end of interval in merged with max value of end
+        # if no-> add current interval to merged
+        # return merged
+        merged = []
+        intervals.sort(key=lambda x:x[0])
+        merged.append(intervals[0])
+        for i in range(1, len(intervals)):
+            start, end = intervals[i][0], intervals[i][1]
+            last_start, last_end = merged[-1][0], merged[-1][1]
+            if last_end>=start:
+                merged[-1][1]=max(end, last_end)
             else:
-                # No overlap, just append pair to result and update pair to next element
-                result.append(pair)
-                pair = intervals[i]
-            i+=1
-        # Append last element in pair
-        result.append(pair)
-        return result
-            
-
-            
-
-        
-
+                merged.append(intervals[i])
+        return merged
