b76e229256781630fe463f2e106f4c95b48795f3
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-02-25 14:20:33 -0500

    Time: 124 ms (25.07%), Space: 20.8 MB (60.3%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,29 +4,18 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        # input: heights list
-        # output: max area between two heights in heights
-        # left and right ptrs
-        # area = maximize->min height(left height, right height)*right-left+1
-        # if left height<right height->left+=1
-        # else right-=1
-        left = 0
-        right = len(height)-1
-        max_area = float("-inf")
-        while left<right:
-            max_area = max(max_area, (right-left)*min(height[left], height[right]))
-            if height[left]<height[right]:
-                left+=1
+        # input: array of heights
+        # output: max area of water between two heights
+        # height-> 0, -ve, only one height -> return 0 area
+        # input size ->10^5 -> O(n) 
+        l = 0
+        r = len(height)-1
+        max_area = 0
+        while l<r:
+            area = (r-l)*min(height[l], height[r])
+            if height[l]<height[r]:
+                l+=1
             else:
-                right-=1
+                r-=1
+            max_area = max(max_area, area)
         return max_area
-
-
-
-
-
-
-
-
-
-       
