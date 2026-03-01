63e77a2f14a305cdcbea86bee12afbb4eb190100
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-03-01 12:18:23 -0500

    Time: 0 ms (100%), Space: 13.1 MB (70.3%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -5,26 +5,36 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        if not nums:
-            return [-1, -1]
         
-        def firstSearch(l,r):
-            while l<r:
-                mid = (l+r)//2
-                if nums[mid]<target:
-                    l = mid+1
-                else: 
-                    r = mid
-            return l if nums[l]==target else -1
-        first = firstSearch(0, len(nums)-1)
-
-        def lastSearch(l, r):
-            while l<r:
-                mid = (l+r+1)//2
-                if nums[mid]>target:
-                    r = mid-1
+        def findFirst(nums, target):
+            left = 0
+            right = len(nums)-1
+            result = -1
+            while left<=right:
+                mid = (left+right)//2
+                if nums[mid]==target:
+                    result = mid
+                    right = mid-1 # shift to keep searching in left side
+                elif target<nums[mid]:
+                    right = mid-1
                 else:
-                    l = mid
-            return r if nums[r]==target else -1
-        last = lastSearch(0, len(nums)-1)   
-        return [first, last]
+                    left = mid+1
+            return result
+
+        def findLast(nums, target):
+            left = 0
+            right = len(nums)-1
+            result = -1
+            while left<=right:
+                mid = (left+right)//2
+                if nums[mid]==target:
+                    result = mid
+                    left = mid+1 # shift to keep searching in right side
+                elif target<nums[mid]:
+                    right = mid-1
+                else:
+                    left = mid+1
+            return result
+        
+        return [findFirst(nums, target), findLast(nums, target)]
+       
