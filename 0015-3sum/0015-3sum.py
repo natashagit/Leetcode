776189fba8ff598754940432514b6e1f8d134a17
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-03-05 21:48:41 -0500

    Time: 810 ms (27.97%), Space: 18.2 MB (96.73%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,33 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        # input: nums
-        # output: triplets that sum up to 0
-        # nums = sorted(nums) nlogn
-        # result = []
-        # [-4, -1, -1, 0, 1,2]
-        # i from 0 to nums len, j at i+1 till j<k, k at end
-        # check that nums[i]!=nums[i-1] 
-        # check if nums[j]+nums[i]+nums[k]==0
-        # if yes, then append triplet to result
-        # now increase j and check nums[j]!=nums[j-1] and j<k for duplicates
-        # if nums[j]+nums[i]+nums[k]>0-> k reduce
-        # if nums[j]+nums[i]+nums[k]<0-> j increase
+        # input: array
+        # output: triplet values that sum to 0 in array
+        nums.sort()
         result = []
-        nums = sorted(nums)
-        for i in range(len(nums)):
-            if i>0 and nums[i]==nums[i-1]:
+        for i in range(len(nums)-2):
+            if nums[i-1]==nums[i] and i>0:
                 continue
-            j=i+1
-            k = len(nums)-1
-            while j<k:
-                if nums[i]+nums[j]+nums[k]>0:
-                    k-=1
-                elif nums[i]+nums[j]+nums[k]<0:
-                    j+=1
+            l = i+1
+            r = len(nums)-1
+            while l<r:
+                if nums[i]+nums[l]+nums[r]>0:
+                    r-=1
+                elif nums[i]+nums[l]+nums[r]<0:
+                    l+=1
                 else:
-                    result.append([nums[i], nums[j], nums[k]])
-                    j+=1
-                    while nums[j]==nums[j-1] and j<k:
-                        j+=1
+                    result.append([nums[i], nums[l], nums[r]])
+                    l+=1
+                    r-=1
+                    while l<r and nums[l]==nums[l-1]:
+                        l+=1
+                    while l<r and nums[r]==nums[r+1]:
+                        r-=1
         return result
