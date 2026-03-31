c5b5773a3640d775075a15ce024dc66b45682d14
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-03-31 07:56:33 -0400

    Time: 0 ms (100%), Space: 12.3 MB (91.56%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -11,28 +11,27 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         # input: list1 and list2
-        # output: merged sorted list
-        # result -> keep a new ListNode()
-        # temp= result
-        # loop through while both lists last
-        # for every value in list1 and list2 check which is smaller
-        # then set the next of temp to the smaller one
-        # in end the add all in list1 and list2
-
-        result = ListNode()
-        temp = result
+        # output: sorted list 
+        dummy = ListNode()
+        head = dummy
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
         while list1 and list2:
             if list1.val<list2.val:
-                temp.next = list1
+                dummy.next = ListNode(list1.val)
                 list1 = list1.next
             else:
-                temp.next = list2
+                dummy.next = ListNode(list2.val)
                 list2 = list2.next
-            temp = temp.next
-        if list1:
-            temp.next = list1
-            
-        if list2:
-            temp.next = list2
-            
-        return result.next
+            dummy = dummy.next
+        while list1:
+            dummy.next = ListNode(list1.val)
+            dummy = dummy.next
+            list1 = list1.next
+        while list2:
+            dummy.next = ListNode(list2.val)
+            dummy = dummy.next
+            list2 = list2.next
+        return head.next
