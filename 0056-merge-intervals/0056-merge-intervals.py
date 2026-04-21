07ae3e60bcadbc9deb3ae24268bffc58a3a2b3ac
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-04-21 03:26:32 -0400

    Time: 6 ms (98.83%), Space: 15.9 MB (91.84%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,26 +4,19 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        # input: intervals [[a,b], [c,d]]
-        # output: merged intervals [[a,d]]
-        # O(nlogn)
-        # if no intervals, return []
-        # set merged=[]
-        # sort all intervals by start time
-        # add first interval to merged
-        # loop through intervals from 2nd onwards
-        # now check if end of interval in merged>=end of current interval
-        # if yes-> overlaps: update end of interval in merged with max value of end
-        # if no-> add current interval to merged
-        # return merged
-        merged = []
-        intervals.sort(key=lambda x:x[0])
-        merged.append(intervals[0])
-        for i in range(1, len(intervals)):
-            start, end = intervals[i][0], intervals[i][1]
-            last_start, last_end = merged[-1][0], merged[-1][1]
-            if last_end>=start:
-                merged[-1][1]=max(end, last_end)
+        intervals = sorted(intervals, key = lambda x:x[0])
+        prev = intervals[0]
+        i = 1
+        result = []
+        while i<len(intervals):
+            next_start, next_end = intervals[i][0], intervals[i][1]
+            prev_start, prev_end = prev[0], prev[1]
+            if next_start<=prev_end:
+                prev = [prev_start, max(prev_end, next_end)]
             else:
-                merged.append(intervals[i])
-        return merged
+                result.append(prev)
+                prev = intervals[i]
+            i+=1
+        result.append(prev)
+        return result
+            
