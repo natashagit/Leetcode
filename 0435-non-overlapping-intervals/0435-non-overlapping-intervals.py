d6ce36269a0b0f98219f1496b1bf1d988a560981
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-04-23 02:43:48 -0400

    Time: 119 ms (87.69%), Space: 45.5 MB (5.45%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,20 +1,23 @@
-class Solution:
-    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        # Sort by start value
-        intervals.sort()
-        end_prev = intervals[0]
-        result = 0
-        # Compare first interval with next to check if overlapping
-        for i in range(1, len(intervals)):
-            # Check if next interval's start is before prev interval's end
-            if intervals[i][0]>=end_prev[1]:
-                # No overlapping, so move ahead and keep the next interval's endpoint
-                end_prev = intervals[i]
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        # |-----|
+        # |--||--||--|
+        # 1--2--3--4
+        # [[1,2],[2,3],[3,4],[1,3]]
+        # sort
+        # [[1,2],[1,3],[2,3],[3,4]]
+        intervals = sorted(intervals, key=lambda x:x[1])
+        check = intervals[0]
+        i = 1
+        remove = 0
+        while i<len(intervals):
+            if intervals[i][0]<check[1]:
+                remove+=1
             else:
-                #overlaps so need to remove 1
-                result+=1
-                # Update prev interval's end point by continuing with the interval that has a shorter end point
-                end_prev[1] = min(end_prev[1], intervals[i][1])
-        return result
-
-                
+                check = intervals[i]
+            i+=1
+        return remove
