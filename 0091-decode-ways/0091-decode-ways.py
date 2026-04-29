29a645bd195063f53225c1e9b50049dd3e3b0bfc
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-04-29 18:39:19 -0400

    Time: 3 ms (42.85%), Space: 12.4 MB (28.2%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,17 +1,22 @@
-class Solution:
-    def numDecodings(self, s: str) -> int:
-        # Initializing
-        dp = {len(s):1}
-        # Loop backwards
-        for i in range(len(s)-1, -1, -1):
-            # If digit is 0
-            if s[i]=="0":
-                dp[i]=0
-            else:
-                dp[i]=dp[i+1]
-            # If two digit starting with 1 or 2"0-6"
-            if i+1<len(s) and (s[i]=="1" or (s[i]=="2" and s[i+1] in "0123456")):
-                # take number of ways for two digit number
-                dp[i]+= dp[i+2]
-        # return total number of ways
-        return dp[0]
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # 2 2 6
+        # 1 1+1 1+2
+        if s[0]=='0':
+            return 0
+        nums = [0]*len(s)
+        nums[0] = 1
+        for i in range(1, len(s)):
+            if s[i]!='0':
+                nums[i]+=nums[i-1]
+            if int(s[i-1:i+1])>=10 and int(s[i-1:i+1])<=26:
+                if i>=2:
+                    nums[i]+=nums[i-2]
+                else: 
+                    nums[i]+=1
+        
+        return nums[-1]
