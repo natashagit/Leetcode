c32df053ff9226999a2bc0faa855603b176bd52a
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-04-29 15:29:49 -0400

    Time: 0 ms (100%), Space: 12.4 MB (55.42%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,9 +1,20 @@
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        # [rob1 rob2 n n+1 ...]
-        rob1, rob2 = 0, 0
-        for n in nums:
-            temp = max(rob1+n, rob2)
-            rob1 = rob2
-            rob2 = temp
-        return temp
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # store max amount of money we can rob up until the index
+        # input: nums array
+        # output: max amount robber - no adjacent houses allowed
+        # [2 1 1 3] = 5 
+        #  2 2 3 5 -> 5
+        # nums[i] = max(nums[i] + nums[i-2], nums[i-1])
+
+        for i in range(1, len(nums)):
+            if i == 1:
+                nums[i] = max(nums[i-1], nums[i])
+            else:
+                nums[i] = max(nums[i] + nums[i-2], nums[i-1])
+        return nums[-1]
+
