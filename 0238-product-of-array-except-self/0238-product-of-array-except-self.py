c327b64a509e92499eafea1dcc3df2c7c31b2659
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-06-22 22:37:20 -0400

    Time: 47 ms (21.52%), Space: 22.2 MB (27.86%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,16 +5,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        result_product = [1 for i in range(len(nums))]
-        prefix = 1
-        for i in range(len(nums)):
-            result_product[i] = prefix
-            prefix = prefix * nums[i]
+        # 0s exist
+        # negatives exist
+        left_product = [1]*len(nums)
+        right_product = [1]*len(nums)
+        run_prod = 1
+        final = []
+        n = len(nums)
+        for i in range(1, n):
+            left_product[i] = left_product[i-1]*nums[i-1]
+            right_product[n-i-1] = right_product[n-i]*nums[n-i]
+        for i in range(n):
+            final.append(left_product[i]*right_product[i])
+        return final
 
-        suffix = 1
-        for i in range(len(nums)-1, -1, -1):
-            result_product[i] = result_product[i]*suffix
-            suffix = suffix * nums[i]
-    
-        
-        return result_product
