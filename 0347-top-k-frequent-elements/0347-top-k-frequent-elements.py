bac6bb73128c0215967ca2f22a2e726e78ec3657
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-06-25 17:53:14 -0400

    Time: 19 ms (17.74%), Space: 20.1 MB (8.17%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,47 +1,45 @@
 class Solution(object):
+    
     def topKFrequent(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: List[int]
         """
-        # Time Complexity: O(nlogk)
-        # Using min heap
-        import heapq # min-heap library
+        # input: array of numbers, integer k
+        # output: top k frequent numbers in the array
+        # k always valid, 1<=k<number of unique elements
+        # same frequency, then order doesn't matter
+        # result not in particular order
+        # brute force
+        # add values into dict with count and sort by values in descending order and take the first k elements
+        # O(nlogn)
+        # optimal approach
+        # keep a hashmap for frequency of each element
+        # as max frequency of any number is size of nums, size of array -> size of nums
+        # make buckets where index shows frequency
+        # add values into the array if it is of that frequency
+        # return those values in descending order
+        # O(n)
+
         from collections import Counter
-        counter = Counter(nums) # creates a dictionary in O(n) with freq
-        heap = []
-        # Push all values with freq onto min heap
-        # Push new val with freq and Pop the value with minimum freq from top of heap until the heap has k elements and then return heap containing k most freq elements
-        for key, val in counter.items():
-            if len(heap)<k:
-                heapq.heappush(heap,(val, key))
-            else:
-                heapq.heappushpop(heap, (val,key))
-        return [h[1] for h in heap]
-            
-        # Time Complexity: O(nlogn)
-        # Create a dict with count for each element in the array
-        dic = {}
+
+        count = Counter(nums) # {1:3, 2:2, 3:1}
+
+        arr = [[] for _ in range(len(nums)+1)]
+        for key,value in count.items():
+            arr[value].append(key)
+        # [[], [3], [2], [1], [], []]
         result = []
-        for i in nums:
-            if i not in dic:
-                dic[i]=1
-            else:
-                dic[i]+=1
-        
-        # Create empty array and append values in position of count
-        freq = [[] for i in range(len(nums)+1)]
-        for n, count in dic.items():
-            freq[count].append(n)
-        
-        for i in range(len(freq)-1, 0 ,-1):
-            for n in freq[i]:
-                result.append(n)
+
+        for i in range(len(arr)-1, 0, -1):
+            for key in arr[i]:
+                result.append(key)
                 if len(result)==k:
                     return result
 
         
-        
+
+
 
 
