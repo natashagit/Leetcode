0e31130e953d1de80bc352ee3c9c0f54bac64745
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-06-25 18:36:00 -0400

    Time: 59 ms (36.35%), Space: 27 MB (46.05%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,28 +1,46 @@
 class Solution(object):
-    # input: unsorted array nums
-    # output: length of longest consecutive sequence of elements in nums
-    # make a set of nums->nums_set
-    # loop through nums
-    # if element-1 not in nums_set->count=1
-        # current=element
-        # while current+1 in nums_set
-            # count+=1
-            # current+=1
-        # longest = max(count, longest)
-
+    
     def longestConsecutive(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        nums_set = set(nums)
-        longest = 0
-        for i in nums_set:
-            if i-1 not in nums_set:
-                current = i
-                count=1
-                while current+1 in nums_set:
+        # input: list of numbers
+        # output: length of longest list containing consecutive numbers
+        # no consecutive elements - return 0
+        # values negative - yes
+        # duplicates - yes
+        # empty array - return 0
+        # size of array 
+        # edge cases: [], [1], 
+        # brute force
+        # loop through nums, while n+1 present->update length and keep max length
+        # checking existence n times for n numbers
+        # O(n^2)
+        # Better approach
+        # Sorting it: [1,2,3,4,100,200] 
+        # can see consecutive number by next
+        # O(nlogn)
+        # Optimal approach
+        # make a set of array
+        # find the highest number and check if num-1 present
+        # 200->199
+        # next highest number
+        # 100-> 99
+        # 4-3-2-1 length+=1 -> maxlength
+
+        maxlength = 0
+        length = 0
+        arr_set = set(nums) #{100,4,200,1,3,2}
+        for num in arr_set: 
+            if num-1 not in arr_set:
+                length = 1
+                current = num
+
+                while current+1 in arr_set:
                     current+=1
-                    count+=1
-                longest = max(longest, count)
-        return longest
+                    length+=1
+            
+                maxlength = max(length, maxlength)
+                
+        return maxlength
