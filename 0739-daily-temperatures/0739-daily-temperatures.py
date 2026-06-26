4c4488d37efd2e3a6e9d606429f940396b784128
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-06-26 19:19:49 -0400

    Time: 175 ms (40.52%), Space: 26.9 MB (62.78%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -4,18 +4,24 @@ class Solution(object):
         :type temperatures: List[int]
         :rtype: List[int]
         """
-        # input: list of temperatures
-        # output: list of number of days to wait for warmer temp
-
-        # stack = []
-        # answer = [0]*len(temperatures)
+        # input: temperatures array
+        # output: array consisting of number of days it takes to get warmer from the ith day
+        # no elements -> []
+        # 1 element -> 0
+        # equal?
+        # brute force
+        # can loop through for every element and check
+        # O(n^2)
+        # Optimal
+        # stack to store indices of days waiting for warmer temp
+        # when u find warmer day -> store the current index-stack[-1]
         
-        stack = []
+        stack = [0]
         answer = [0]*len(temperatures)
-        for i in range(len(temperatures)):
+        for i in range(1, len(temperatures)):
             while stack and temperatures[i]>temperatures[stack[-1]]:
-                prev = stack.pop()
-                answer[prev] = i-prev
+                answer[stack[-1]] = i-stack[-1]
+                stack.pop()
+            
             stack.append(i)
-        
         return answer
