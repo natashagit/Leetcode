85d6cfd9dff52b2a9c77fd2df1a67e86cc8d7e6b
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-06-26 18:26:29 -0400

    Time: 227 ms (18.81%), Space: 15.6 MB (47.3%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -5,40 +5,56 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        # input: s and t
-        # output: smallest substring of s which contains s
-
-        # keep two hashmaps with freq of t elements and freq of window elements
-        # keep have and need counts
-
-        if t == "":
+        # input: s="ADOBECODEBANC" and t="ABCB"
+        # output: shortest substring with chars of t in s: "BANC"
+        # t not in s-> ""
+        # empty s -> ""
+        # t-> 1 char; "A" if in s
+        # all chars uppercase english letters?
+        # brute force
+        # Loop through s and check if every substring contains all chars of t
+        # O(n^2)
+        # Optimal
+        # sliding window
+        # loop through s with two pointers
+        # right pointer moves and keeps adding char to window
+        # track if values meeting validity of chars in t ->store length
+        # once meets all of t 
+        # shrink window from left
+        # update minlength
+        if not s or not t:
             return ""
-        countT = {}
-        window = {}
-        for i in t:
-            countT[i] = countT.get(i, 0)+1
-        have, need = 0, len(countT)
-        result = [-1,-1] # for [l, r]
-        result_length = float("inf")
-        l = 0
-        for r in range(len(s)):
-            window[s[r]]= window.get(s[r], 0)+1
-
-            if s[r] in countT and window[s[r]]==countT[s[r]]:
-                have+=1
+        from collections import Counter, defaultdict
+        dict_t = Counter(t)
+        window = defaultdict(int)
+        left= 0
+        formed = 0
+        required = len(dict_t)
+        min_len = float("inf")
+        result = ""
+        for right in range(len(s)):
+            window[s[right]]+=1
+            if s[right] in dict_t and dict_t[s[right]]==window[s[right]]:
+                formed+=1
             
-            while have == need:
-                # update result
-                if (r-l+1)<result_length:
-                    result = [l, r]
-                    result_length=r-l+1
-                # pop from left of window
-                window[s[l]]-=1
-                # if while removing we made it less than what it needed to be
-                if s[l] in countT and window[s[l]]<countT[s[l]]:
-                    have-=1
-                l+=1
-        l, r = result[0], result[1]
-        if result_length!=float("inf"):
-            return s[l:r+1]
-        return ""
+            while formed==required:
+                if right-left+1<min_len:
+                    min_len = right-left+1
+                    result = s[left:right+1]
+                
+                window[s[left]]-=1
+                if s[left] in dict_t and window[s[left]]<dict_t[s[left]]:
+                    formed-=1
+                left+=1
+        return result
+            
+
+
+            
+
+            
+
+            
+            
+        
+        
