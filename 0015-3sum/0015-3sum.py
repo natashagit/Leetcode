e3ec7dcf8226308e1fec13d404e06fc7be3d18f0
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-06-25 23:29:30 -0400

    Time: 794 ms (30.82%), Space: 18.3 MB (53.87%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,25 +5,48 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         # input: array
-        # output: triplet values that sum to 0 in array
+        # output: triplets list that sum up to 0 from array
+        # no duplicates
+        # order doesnt matter
+        # less than 3 elements -> []
+        # for every possible triplet use 3 nested loops
+        # O(n^3)
+        # Optimal
+        # sort the array [-4, -1, -1, 0, 1, 2]
+        # loop through the array 
+        # assign the left pointer and right pointer
+        # loop while left<right
+        # sum up and see if 0
+        # if 0 then return the indices
+            # move left ahead and check its not equal to prev
+        # if >0 move right 
+        # if <0 move left
+        # O(n^2)
         nums.sort()
         result = []
-        for i in range(len(nums)-2):
-            if nums[i-1]==nums[i] and i>0:
+        for i in range(len(nums)-2): # [-4, -1, -1, 0, 1, 2]
+            if i>0 and nums[i-1]==nums[i]:
                 continue
-            l = i+1
-            r = len(nums)-1
-            while l<r:
-                if nums[i]+nums[l]+nums[r]>0:
-                    r-=1
-                elif nums[i]+nums[l]+nums[r]<0:
-                    l+=1
+            left = i+1 #1
+            right = len(nums)-1 #5
+            while left<right:
+                if nums[left]+nums[right]+nums[i]>0:
+                    right-=1
+                elif nums[left]+nums[right]+nums[i]<0:
+                    left+=1
                 else:
-                    result.append([nums[i], nums[l], nums[r]])
-                    l+=1
-                    r-=1
-                    while l<r and nums[l]==nums[l-1]:
-                        l+=1
-                    while l<r and nums[r]==nums[r+1]:
-                        r-=1
+                    result.append([nums[i], nums[left], nums[right]])
+                    left+=1
+                    right-=1
+                    while left<right and nums[left]==nums[left-1]:
+                        left+=1
+                    while left<right and nums[right]==nums[right+1]:
+                        right-=1
+                    
         return result
+        
+
+
+
+
+
