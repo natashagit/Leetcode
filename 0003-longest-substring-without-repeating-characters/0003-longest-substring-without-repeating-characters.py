50a7469f0fce66149dba197647a105ce04f19228
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-06-26 14:29:14 -0400

    Time: 27 ms (44.85%), Space: 12.4 MB (90.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,25 +4,33 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # input: string
-        # output: length of longest substring without duplicates
-
-        # sliding window keeping only the substring with non duplicate chars
-        # keep max count of longest substring
-        # create set to store chars
-        # keep pointer at 0
-        # loop through string
-        # while char present in set remove first element of string in set
-        # add new char to set
-        # store into max_length the length of substring
-            
-        max_length = 0
-        str_set = set()
-        l=0
-        for i in range(len(s)):
-            while s[i] in str_set:
-                str_set.remove(s[l])
-                l+=1
-            str_set.add(s[i])
-            max_length = max(max_length, i-l+1)
-        return max_length
+        # input: string with duplicate chars
+        # output: length of longest substring without duplicate chars
+        # empty string -> 0
+        # one char -> 1
+        # numbers or special chars - yes
+        # size of string
+        # output only the length, not the string
+        # abcbacbb
+        # brute force
+        # loop through string   
+        # have a dictionary for each starting char  {a:1, b:1, c:1}
+        # store length of string by taking number of keys in dictionary
+        # O(n^2)
+        # Optimal 
+        # sliding window approach
+        # we keep an index at start and second that moves through as sliding and
+        # add to set each char
+        # we store length of set in maxlength
+        # when we see char already present in set, we remove leftmost char until char not already present from set
+        string_set = set()
+        length = 0
+        i = j = 0
+        while j<len(s):
+            while s[j] in string_set:
+                string_set.remove(s[i])
+                i+=1
+            string_set.add(s[j])
+            length = max(length, len(string_set))
+            j+=1
+        return length
