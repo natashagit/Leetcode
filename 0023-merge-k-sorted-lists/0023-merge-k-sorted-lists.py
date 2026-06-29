40cfc6919eed052b75ec46491d1b34f9d6d04bbf
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-06-29 17:09:58 -0400

    Time: 3029 ms (7.79%), Space: 18.1 MB (80.33%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,41 +1,35 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists)==0:
-            return None
-        while len(lists)>1:
-            mergedList = []
-            for i in range(0, len(lists), 2):
-                # Taking two lists at a time
-                list1 = lists[i]
-                list2 = lists[i+1] if i+1<len(lists) else None
-                # Merging the two lists and appending them
-                mergedList.append(self.mergeTwoLists(list1, list2))
-            # Finally putting it into the lists
-            lists = mergedList
-        # returning the list
-        return lists[0]
-    
-    def mergeTwoLists(self, list1, list2):
-        dummy = ListNode()
-        tail = dummy
-        while list1 and list2:
-            if list1.val<list2.val:
-                tail.next = list1
-                list1 = list1.next
-            else:
-                tail.next = list2
-                list2 = list2.next
-            tail = tail.next
-        if list1:
-            tail.next = list1
-        if list2:
-            tail.next = list2
-        return dummy.next
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[Optional[ListNode]]
+        :rtype: Optional[ListNode]
+        """
+        # brute force - use array- sort it and then build ll: O(nlogn)
+        def mergeTwo(list1, list2):
+            dummy = ListNode(0)
+            curr = dummy
+            while list1 and list2:
+                if list1.val<list2.val:
+                    curr.next=list1
+                    list1 = list1.next
+                else:
+                    curr.next=list2
+                    list2 = list2.next
+                
+                curr= curr.next
+            if list1:
+                curr.next = list1
+            if list2:
+                curr.next = list2
 
-
-
+            return dummy.next
+        merged = None
+        for l in lists:
+            merged = mergeTwo(merged, l)
+        return merged
+        
