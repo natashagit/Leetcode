36785d255ad93ac2554972e320a9c4fa809f5dfc
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-06-30 13:55:12 -0400

    Time: 4 ms (38.77%), Space: 17.1 MB (27.81%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -10,16 +10,29 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: bool
         """
-        # Use recursive fn to check
-        def valid(node, left, right):
-            # If empty, return true    
-            if not node:
-                return True
-            # If out of bounds, return false
-            if not(node.val<right and node.val>left):
-                return False
-            # If it goes left or right, bounds accordingly set
-            return (valid(node.left, left, node.val) and (valid(node.right, node.val, right)))
-        # Pass the initial root and bounds to recursively check
-        return valid(root, float("-inf"), float("inf"))
+        # BST: left elements less than node val, right elements> node val
+        # If we do normal left and right subtree check, we may miss out on situations where right subtree's left element less than node
+        # Brute force approach
+        # Need to check if everything on left of node smaller and vice versa
+        # DFS for every node -> O(n^2)
+        # Optimal approach
+        # we can set valid ranges for each node to check their value based on the parent and if its left or right node
+        # root -> -infinity to infinity
+        # root.left -> -infinity to root.val
+        # root.right -> root.val to infinity
+        # Complexity: O(n)
+        # create a function to check value of node with boundaries recursively
+        # check if the node's value not between the boundaries -> return false
+        # recursively call the function on node's left and node's right values
+        # call the function with initial boundaries on node
         
+        def valid_node(node, low, high):
+            if node is None:
+                return True
+
+            if not (low<node.val<high):
+                return False
+                
+            return valid_node(node.left, low, node.val) and valid_node(node.right, node.val, high)
+
+        return valid_node(root, float("-inf"), float("inf"))
