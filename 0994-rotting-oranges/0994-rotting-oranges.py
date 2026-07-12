2b47d93b1f4b08d83064f8a9112757636642bf2f
Natasha Sebastian <40355715+natashagit@users.noreply.github.com>
2026-07-11 22:56:22 -0400

    Time: 10 ms (16.31%), Space: 12.5 MB (22.47%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -4,42 +4,42 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        # input: grid containing 2:rotten, 1:fresh, 0: no oranges
-        # output: return steps until all oranges traversed
-        # Use BFS for level by level traversal
-        # need to add all rotten oranges to queue
-        # each level-1 minute - time increment
-        # track fresh oranges - everytime rots- decrement
-        # if fresh oranges> 0 in end- return -1
-        # else return time
-        rows, cols = len(grid), len(grid[0])
-        fresh_oranges = 0
+        # put all rotten oranges in queue as (r,c,time it becomes rotten-initally 0)
+        # keep a visited set to store the r, c of the rotten oranges
+        # do a bfs and keep updating time as new oranges get rotten and add r,c of new oranges to visited
+        # if fresh oranges still left -> return -1
+        # return the maximum time it takes to make all oranges rotten
+        
+        
         from collections import deque
-        q = deque()
+
+        rotten_oranges = deque()
+        time = 0
+        visited = set()
+        rows, cols = len(grid), len(grid[0])
+
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c]==1:
-                    fresh_oranges+=1
-                elif grid[r][c]==2:
-                    q.append((r,c))
-        if fresh_oranges==0:
-            return 0
-        time = 0
-        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+                if grid[r][c]==2:
+                    rotten_oranges.append((r,c, 0))
+                    visited.add((r,c))
+        directions = [[0,1], [1,0], [0,-1], [-1, 0]]
+        while rotten_oranges:
+            r,c,t = rotten_oranges.popleft()
+            time = max(t, time)
+            for dr,dc in directions:
+                new_r = r+dr
+                new_c = c+dc
+                if new_r>=0 and new_r<rows and new_c>=0 and new_c<cols and grid[new_r][new_c]==1 and (new_r, new_c) not in visited:
+                    rotten_oranges.append((new_r, new_c, t+1))
+                    visited.add((new_r, new_c))
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]==1 and (r,c) not in visited:
+                    return -1 
+        
+        return time
 
-        while q and fresh_oranges>0:
-            for _ in range(len(q)):
-                r,c = q.popleft()
-                for dr, dc in directions:
-                    nr, nc = dr+r, dc + c
-                    if nr>=0 and nr<rows and nc>=0 and nc<cols and grid[nr][nc]==1:
-                        grid[nr][nc]=2
-                        fresh_oranges-=1
-                        q.append((nr,nc))
-            time+=1
-        
-        return time if fresh_oranges==0 else -1
-        
-        
 
         
